03d7ad51400da7062b86d407888b2129966860a0
bronzo97 <danilobronzo97@hotmail.it>
2022-09-19 11:02:39 +0200

    adds api to select the restaurant

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,17 @@
 <template>
     <div>    
-        <h1>Ciao</h1>
+        <h1>Home</h1>
         <img src="/storage/img/categorie/categoria_messicano.png">
+
+        <Restaurant></Restaurant>
+
     </div>
 </template>
 
 <script>
+    import Restaurant from './Restaurant.vue';
+
     export default {
-        
-    }
+    components: { Restaurant }
+}
 </script>

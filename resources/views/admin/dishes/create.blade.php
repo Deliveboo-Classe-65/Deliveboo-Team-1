97ace54ca2c0a28825cb559069aad17d99cf562b
NickAstley <andreanicastro1994@gmail.com>
2022-09-20 18:44:07 +0200

    Restyled Dish crud

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -2,88 +2,90 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <div class="d-flex justify-content-between align-items-center">
-                <h1>Crea piatto</h1>
-                <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg> Tutti i piatti
-                </a>
-            </div>
+  <div class="d-flex justify-content-between align-items-center">
+      <h1>Crea piatto</h1>
+      <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg> Tutti i piatti
+      </a>
+  </div>
 
-            <div>
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-            </div>
+  <div>
+      @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+      @endif
+  </div>
 
-            <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label for="name" class="form-label">Nome</label>
-                    <input type="text" required minlength="5" id="name" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Inserisci il nome del piatto" value="{{ old('name') }}">
-                    @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+  <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
+      @csrf
 
-                <div class="form-group">
-                  <label for="price" class="form-label">Prezzo</label>
-                  <input type="number" id="price" name="price" step="0.01" class="form-control @error('price') is-invalid @enderror" placeholder="Inserisci il prezzo del piatto" value="{{ old('price') }}">
-                  @error('price')
-                  <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                  <label for="img_file" class="form-label">Immagine</label>
-      
-                  <div class="d-flex">
-                    <input type="file" name="image" class="form-control-file @error('image') is-invalid @enderror"
-                      id="img_file">
-                  </div>
-                  @error('image')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                    <label for="description" class="form-label">Descrizione</label>
-                    <textarea name="description" id="description" class="form-control @error('description') is-invalid @enderror" rows="10" placeholder="Inserisci una descrizione del piatto">{{ old('description') }}</textarea>
-                    @error('description')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                @foreach ($types as $type)
-                  <div class="form-check">
-                    <input class="form-check-input" {{ in_array($type->id, old("types", [])) ? 'checked' : '' }} name="types[]" type="checkbox" value="{{ $type->id }}" id="{{"flexCheckDefault" . $type->id}}">
-                    <label class="form-check-label" for={{"flexCheckDefault" . $type->id}}>
-                      {{ $type->name }}
-                    </label>
-                  </div>
-                @endforeach
-
-                <div class="form-check">
-                  <input class="form-check-input" {{ old("visibility") || $errors->count() === 0 ? 'checked' : '' }} name="visibility" type="checkbox" value="1" id="flexCheckDefault">
-                  <label class="form-check-label" for="flexCheckDefault">
-                    Visibile
-                  </label>
-                </div>
-
-                <div class="form-group mt-3">
-                    <button type="submit" class="btn btn-success">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg> Crea piatto
-                    </button>
-                </div>
-            </form>
+      <div class="row mb-3">
+        <div class="col-md-8">
+            <label for="name">Nome</label>
+            <input type="text" required minlength="5" id="name" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Inserisci il nome del piatto" value="{{ old('name') }}">
+            @error('name')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
-    </div>
+        <div class="col-md-4">
+          <label for="price">Prezzo</label>
+          <input type="number" id="price" name="price" step="0.01" class="form-control @error('price') is-invalid @enderror" placeholder="Inserisci il prezzo del piatto" value="{{ old('price') }}">
+          @error('price')
+          <div class="invalid-feedback">{{ $message }}</div>
+          @enderror
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="img_file">Immagine</label>
+
+        <div class="d-flex">
+          <input type="file" name="image" class="form-control @error('image') is-invalid @enderror" id="img_file">
+        </div>
+        @error('image')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+      </div>
+
+      <div class="mb-3">
+        <label for="description">Descrizione</label>
+        <textarea name="description" id="description" class="form-control @error('description') is-invalid @enderror" rows="3" placeholder="Inserisci una descrizione del piatto">{{ old('description') }}</textarea>
+        @error('description')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+      </div>
+
+      <div class="mb-3">
+        <fieldset class="form-control">
+          <legend class="fs-6">Seleziona una o più categorie</legend>
+          @foreach ($types as $type)
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" {{ in_array($type->id, old("types", [])) ? 'checked' : '' }} name="types[]" type="checkbox" value="{{ $type->id }}" id="{{"flexCheckDefault" . $type->id}}">
+              <label class="form-check-label" for={{"flexCheckDefault" . $type->id}}>
+                {{ $type->name }}
+              </label>
+            </div>
+          @endforeach
+        </fieldset>
+      </div>
+
+      <div class="form-check form-switch mb-3">
+        <input class="form-check-input" {{ old("visibility") || $errors->count() === 0 ? 'checked' : '' }} role="switch" name="visibility" type="checkbox" value="1" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          Visibile
+        </label>
+      </div>
+
+      <div class="form-group">
+          <button type="submit" class="btn btn-success">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg> Crea piatto
+          </button>
+      </div>
+  </form>
 </div>
 @endsection
--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -21,39 +21,43 @@
   @csrf 
   
   <div class="container">
-    <h1>Modifica piatto</h1>
+    <div class="d-flex justify-content-between align-items-center">
+      <h1>Modifica piatto</h1>
+      <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg> Tutti i piatti
+      </a>
+    </div>
     <form action="{{ route('admin.dishes.update', $dish->id) }}" method="post" enctype="multipart/form-data">
       @csrf
 
       @method('PUT')
 
-      <div class="mb-3">
-        <label for="nameInput" class="form-label">Nome</label>
-        <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name"
-          id="nameInput" value="{{ $dish->name }}">
-        <div class="invalid-feedback">
-          {{ $errors->first('name') }}
+      <div class="row mb-3">
+        <div class="col-md-8">
+          <label for="nameInput">Nome</label>
+          <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" id="nameInput" value="{{ $dish->name }}">
+          <div class="invalid-feedback">
+            {{ $errors->first('name') }}
+          </div>
+        </div>
+        <div class="col-md-4">
+          <label for="priceInput">Prezzo</label>
+          <input type="number" class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" name="price" id="priceInput" value="{{ $dish->price }}">
+          <div class="invalid-feedback">
+            {{ $errors->first('price') }}
+          </div>
         </div>
       </div>
 
       <div class="mb-3">
-        <label for="priceInput" class="form-label">Prezzo</label>
-        <input type="number" class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" name="price"
-          id="priceInput" value="{{ $dish->price }}">
-        <div class="invalid-feedback">
-          {{ $errors->first('price') }}
-        </div>
-      </div>
-
-      <div class="form-group">
-        <label for="img_file" class="form-label">Immagine</label>
+        <label for="img_file" >Immagine</label>
 
         <div class="d-flex">
           @if ($dish->image)
-          <img class="img-thumbnail" style="width: 150px" src="{{ asset('storage/' . $dish->image) }}">
+            <img class="img-thumbnail" style="width: 150px" src="{{ asset('storage/' . $dish->image) }}">
           @endif
           
-          <input type="file" name="image" class="form-control-file @error('image') is-invalid @enderror" id="img_file">
+          <input type="file" name="image" class="form-control @error('image') is-invalid @enderror" id="img_file">
         </div>
         @error('image')
           <div class="invalid-feedback">{{ $message }}</div>
@@ -61,25 +65,29 @@
       </div>
 
       <div class="mb-3">
-        <label for="descInput" class="form-label">Descrizione</label>
-        <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="descInput"
-          rows="3">{!! $dish->description  !!}</textarea>
+        <label for="descInput" >Descrizione</label>
+        <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="descInput" rows="3">{!! $dish->description  !!}</textarea>
         <div class="invalid-feedback">
           {{ $errors->first('description') }}
         </div>
       </div>
 
-      @foreach ($types as $type)
-      <div class="form-check">
-          <input class="form-check-input" {{ checked($type, $dish, old("types"), $errors) }} name="types[]" type="checkbox" value="{{ $type->id }}" id="{{"flexCheckDefault" . $type->id}}">
-          <label class="form-check-label" for={{"flexCheckDefault" . $type->id}}>
-            {{ $type->name }}
-          </label>
+      <div class="mb-3">
+        <fieldset class="form-control">
+          <legend class="fs-6">Seleziona una o più categorie</legend>
+            @foreach ($types as $type)
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" {{ checked($type, $dish, old("types"), $errors) }} name="types[]" type="checkbox" value="{{ $type->id }}" id="{{"flexCheckDefault" . $type->id}}">
+                <label class="form-check-label" for={{"flexCheckDefault" . $type->id}}>
+                  {{ $type->name }}
+                </label>
+            </div>
+            @endforeach
+        </fieldset>
       </div>
-      @endforeach
-      
-      <div class="form-check">
-        <input class="form-check-input" {{ $errors->count() === 0 && $dish->visibility === 1 || old("visibility") ? 'checked' : '' }} name="visibility" type="checkbox" value="1" id="flexCheckDefault">
+
+      <div class="form-check form-switch mb-3">
+        <input class="form-check-input" {{ $errors->count() === 0 && $dish->visibility === 1 || old("visibility") ? 'checked' : '' }} role="switch" name="visibility" type="checkbox" value="1" id="flexCheckDefault">
         <label class="form-check-label" for="flexCheckDefault">
           Visibile
         </label>
@@ -90,6 +98,5 @@
         <a href="{{ route('admin.dishes.index') }}" class="btn btn-danger">Annulla</a>
       </div>
     </form>
-
   </div>
 @endsection

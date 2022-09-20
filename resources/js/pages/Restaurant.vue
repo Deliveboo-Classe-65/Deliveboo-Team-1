56b469fe889ca57c2ba76ee0c0a0b9da4b5d465c
bronzo97 <danilobronzo97@hotmail.it>
2022-09-20 12:34:42 +0200

    adds menu and restaurant component

--- resources/js/pages/Restaurant.vue
+++ resources/js/pages/Restaurant.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
     <div class="container">
         <div class="row">
             <!-- Restaurant image -->
@@ -22,6 +23,35 @@
             <div class="col col-4"></div>
         </div>
     </div>
+=======
+    <section class="restaurant py-3">
+        <div class="container">
+            <div class="row restaurant-row">
+                <!-- Restaurant image -->
+                <div class="col col-4 my-3">
+                    <img src="../../../public/img/bk.jpeg" alt="Burger King" class="w-100">
+                </div>
+                <!-- Restaurant name and description -->
+                <div class="col col-6 my-3">
+                    <h1>Burger King</h1>
+                    <div class="d-flex categories">
+                        <p class="delivery-time">20 - 25 minuti</p>
+                        <p class="me-3">Americano</p>
+                        <p class="me-3">Hamburger</p>
+                    </div>
+                    <h5>IL NOSTRO GUSTO? SUPERIORE. BURGER KING®. A MODO TUO.</h5>
+                </div>
+                <!-- Restaurant time to deliver -->
+                <div class="col col-2 my-3 d-flex">
+                    <div class="icon">
+                        <img src="../../../public/img/biker_icon.png" alt="biker icon" class="w-100">
+                    </div>
+                    <p>Consegna: 20 - 25 minuti</p>
+                </div>
+            </div>
+        </div>
+    </section>
+>>>>>>> adds-restaurant-and-menù
 </template>
 
 <script>
@@ -46,4 +76,27 @@
         }
     }
 
+<<<<<<< HEAD
 </script>
+=======
+</script>
+
+<style lang="scss">
+
+.restaurant {
+        background-color: #fff;
+        border-bottom: 1px solid rgba(0,0,0,.08);
+    }
+.restaurant-row {
+    height: 100%;
+    min-height: 244px;
+    display: flex;
+}
+
+.icon {
+        width: 20px;
+        height: 20px;
+        margin-right: 4px;
+    }
+</style>
+>>>>>>> adds-restaurant-and-menù

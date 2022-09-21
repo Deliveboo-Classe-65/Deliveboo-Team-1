6e6620e5bd4771b2936c4f57c41882d81ee85bcb
bronzo97 <danilobronzo97@hotmail.it>
2022-09-21 12:29:51 +0200

    update resturant show

--- resources/js/pages/Restaurant.vue
+++ resources/js/pages/Restaurant.vue
@@ -1,9 +1,31 @@
 <template>
-    <div class="container">
-
-        <h1>{{user.name}}</h1>
-    </div>
-
+    <section class="restaurant py-3">
+        <div class="container">
+            <div class="row restaurant-row">
+                <!-- Restaurant image -->
+                <div class="col col-4 my-3">
+                    <img src="../../../public/img/bk.jpeg" alt="Burger King" class="w-100">
+                </div>
+                <!-- Restaurant name and description -->
+                <div class="col col-6 my-3">
+                    <h1>{{user.name}}</h1>
+                    <div class="d-flex categories">
+                        <p class="delivery-time">20 - 25 minuti</p>
+                        <p class="me-3">Americano</p>
+                        <p class="me-3">Hamburger</p>
+                    </div>
+                    <h5>IL NOSTRO GUSTO? SUPERIORE. BURGER KING®. A MODO TUO.</h5>
+                </div>
+                <!-- Restaurant time to deliver -->
+                <div class="col col-2 my-3 d-flex">
+                    <div class="icon">
+                        <img src="../../../public/img/biker_icon.png" alt="biker icon" class="w-100">
+                    </div>
+                    <p>Consegna: 20 - 25 minuti</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 </template>
@@ -30,4 +52,23 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+
+    .restaurant {
+            background-color: #fff;
+            border-bottom: 1px solid rgba(0,0,0,.08);
+        }
+    .restaurant-row {
+        height: 100%;
+        min-height: 244px;
+        display: flex;
+    }
+    
+    .icon {
+            width: 20px;
+            height: 20px;
+            margin-right: 4px;
+        }
+    </style>
 
